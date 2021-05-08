8aeba19acb540db7562952b03da3235c987eb684
Danny Kaplan <kaplan@macalester.edu>
2021-05-08 18:53:19 -0500

    Adding initial files

---
+++ _common.R
@@ -0,0 +1,26 @@
+library(mosaic)
+library(mosaicCalc)
+library(math141Z) # REPLACE THIS WHEN PACKAGES ARE RE-ALIGNED
+
+objective_list <- list()
+
+add_objective <- function(ID, text) {
+  objective_list[[length(objective_list) + 1]] <<-
+                      list(ID = ID, text = text)
+}
+
+state_objective <- function(ID, text) {
+  add_objective(ID, text)
+  format_objective(list(ID=ID, text=text))
+}
+
+format_objective <- function(obj) {
+  knitr::asis_output(glue::glue("#. **[{obj$ID}]** *{obj$text}*\n\n"))
+}
+
+show_objectives <- function() {
+  lapply(objective_list, format_objective) %>%
+    unlist() %>%
+    paste(collapse="\n") %>%
+    knitr::asis_output()
+}
