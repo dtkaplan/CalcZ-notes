07e8c44b48acac8f9b66a748c9380d33f64d3423
Danny Kaplan <kaplan@macalester.edu>
2021-07-08 07:28:26 -0500

    Pulled in changed files from main branch

---
+++ _place_marks.R
@@ -0,0 +1,24 @@
+# Insert marks in a chapter file
+
+remark_chapter <- function(fname, start, skip=5) {
+  text <- readLines(fname)
+  long_lines <- nchar(text) > 200
+  blank_lines <- !grepl("[a-zA-Z0-9`]", text)
+  chunk_lines <- grepl("^[```|:::]", text)
+
+
+  long_lines <- long_lines & ! chunk_lines
+  long_lines <- long_lines[-length(long_lines)]
+  blank_lines <- blank_lines[-1]
+
+  # Long lines followed by a blank line
+  candidate_lines <- which(long_lines & blank_lines)
+
+  for (k in 1:length(candidate_lines)) {
+    mark_text <- glue::glue("`r mark({start + skip*(k-1)})`")
+    text[candidate_lines[k]] <- paste(text[candidate_lines[k]], mark_text)
+  }
+
+  writeLines(text, con=fname)
+  return(start + skip*length(candidate_lines))
+}
